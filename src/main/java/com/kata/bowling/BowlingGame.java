c5473ae8292ff5d0bc9e25e771711997807dac02
2019_SOW_DEV_019 <2019sowdev019@gmail.com>
2019-05-09 01:53:20 +0200

    Should return score as zero when all the rolls with no pins knocked

---
+++ src/main/java/com/kata/bowling/BowlingGame.java
@@ -0,0 +1,7 @@
+package com.kata.bowling;
+
+public class BowlingGame {
+    public int score() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/kata/bowling/BowlingGameTest.java
@@ -0,0 +1,14 @@
+package com.kata.bowling;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BowlingGameTest {
+    @Test
+    public void shouldReturnZeroWhenAllRollsZero() {
+        BowlingGame game = new BowlingGame();
+
+        assertEquals(0,game.score());
+    }
+}
