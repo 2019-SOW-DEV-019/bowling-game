0195e2f313571dabef29c5a4974f8aa6d918b5f7
2019_SOW_DEV_019 <2019sowdev019@gmail.com>
2019-05-09 02:01:48 +0200

    Should return 20 when all the rolls with 1 pin knocked

--- src/main/java/com/kata/bowling/BowlingGame.java
+++ src/main/java/com/kata/bowling/BowlingGame.java
@@ -1,7 +1,13 @@
 package com.kata.bowling;
 
+import java.util.List;
+
 public class BowlingGame {
-    public int score() {
-        return 0;
+    public int score(List<String> rolls) {
+        int score = 0;
+        for (String roll : rolls){
+            score += Integer.valueOf(roll);
+        }
+        return score;
     }
 }
--- src/test/java/com/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kata/bowling/BowlingGameTest.java
@@ -2,6 +2,9 @@ package com.kata.bowling;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
@@ -9,7 +12,22 @@ public class BowlingGameTest {
     @Test
     public void shouldReturnZeroWhenAllRollsZero() {
         BowlingGame game = new BowlingGame();
+        List<String> rolls = new ArrayList<>();
+        for (int rollCount = 0; rollCount < 20; rollCount++) {
+            rolls.add("0");
+        }
 
-        assertEquals(0, game.score());
+        assertEquals(0, game.score(rolls));
+    }
+
+    @Test
+    public void shouldReturn20WhenAllRollswith1PinDown() {
+        BowlingGame game = new BowlingGame();
+        List<String> rolls = new ArrayList<>();
+        for (int rollCount = 0; rollCount < 20; rollCount++) {
+            rolls.add("1");
+        }
+
+        assertEquals(20, game.score(rolls));
     }
 }
