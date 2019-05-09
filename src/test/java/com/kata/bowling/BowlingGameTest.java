830bab4220468e03acb53f9121abc1b48ecc0146
2019_SOW_DEV_019 <2019sowdev019@gmail.com>
2019-05-09 02:06:47 +0200

    Refactor - Extracting the buildRolls method and introduction of setUp method

--- src/test/java/com/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kata/bowling/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package com.kata.bowling;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,25 +10,32 @@ import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
 
+    BowlingGame game;
+    List<String> rolls;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+        rolls = new ArrayList<>();
+    }
+
     @Test
     public void shouldReturnZeroWhenAllRollsZero() {
-        BowlingGame game = new BowlingGame();
-        List<String> rolls = new ArrayList<>();
-        for (int rollCount = 0; rollCount < 20; rollCount++) {
-            rolls.add("0");
-        }
+        buildRolls("0");
 
         assertEquals(0, game.score(rolls));
     }
 
     @Test
     public void shouldReturn20WhenAllRollswith1PinDown() {
-        BowlingGame game = new BowlingGame();
-        List<String> rolls = new ArrayList<>();
-        for (int rollCount = 0; rollCount < 20; rollCount++) {
-            rolls.add("1");
-        }
+        buildRolls("1");
 
         assertEquals(20, game.score(rolls));
+    }
+
+    private void buildRolls(String noOfRollsToAdd) {
+        for (int rollCount = 0; rollCount < 20; rollCount++) {
+            rolls.add(noOfRollsToAdd);
+        }
     }
 }
